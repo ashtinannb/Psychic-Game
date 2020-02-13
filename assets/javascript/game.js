f1dee3eef7e84a5f68cbb4972239e85d8b5dc5c4
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-02-13 01:40:37 -0600

    started fresh. added alphabet array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
 document.addEventListener("keyup", function(event){
     console.log(event.key)
 })
+
+
+var countwins = 0;
+var countLoss = 0;
+var countguessLeft = 0;
+var countguessesNow = 0;
+
+$("#wins").on("click", function() {
+    alert("wins");
+    countwins++;
+    $("#wins-count").text(countwins);
+});
