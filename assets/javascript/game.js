ce8f8326cbe2be42826cca350a1f583cf92862c1
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-02-13 02:25:00 -0600

    added keyup event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,29 @@
+//alphabet array
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessLeft = 10;
+var guessed = 0;
+
+//generate random letter
+var randomLetter = Math.floor(Math.random() * alphabet.length);
+var computerChoice = alphabet[randomLetter];
+console.log("The Computer Chooses" + computerChoice);
+
+//reset
+function reset(){
+    guessLeft= 10;
+    guessed= [];
+}
+
+//keyup event
 document.addEventListener("keyup", function(event){
     console.log(event.key)
 })
 
+//user selects letter
 
-var countwins = 0;
-var countLoss = 0;
-var countguessLeft = 0;
-var countguessesNow = 0;
+//check to see if letter matches computer choice
 
-$("#wins").on("click", function() {
-    alert("wins");
-    countwins++;
-    $("#wins-count").text(countwins);
-});
+//if the user gets the right letter
