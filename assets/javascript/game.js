e02bdad72de011218886b8b434ac89addc5a7308
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-03-08 13:39:14 -0500

    finished functionality of webpage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,58 @@
-//alphabet array
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var myChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 
 var wins = 0;
 var losses = 0;
-var guessLeft = 10;
-var guessed = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var myGuess = [];
 
-//generate random letter
-var randomLetter = Math.floor(Math.random() * alphabet.length);
-var computerChoice = alphabet[randomLetter];
-console.log("The Computer Chooses" + computerChoice);
 
-//reset
-function reset(){
-    guessLeft= 10;
-    guessed= [];
+
+window.onload = function() {
+	var compGuess = myChoices[Math.floor(Math.random() * myChoices.length)];
+	myGuess.push(myGuess);
+	console.log(myGuess[0]);
 }
 
-//keyup event
-document.addEventListener("keyup", function(event){
-    console.log(event.key)
-})
 
-//user selects letter
 
-//check to see if letter matches computer choice
+document.onkeyup = function(event) {
+	var yourGuess = event.key;
+	lettersGuessed.push(yourGuess);
+	console.log(myGuess[0]);
 
-//if the user gets the right letter
+if ((yourGuess === myGuess[0]) && (guessesLeft > 0)) {
+	wins++;
+	guessesLeft = 9;
+	lettersGuessed.length = 0;
+	myGuess.length = 0;
+	var compGuess = myChoices[Math.floor(Math.random() * myChoices.length)];
+	myGuess.push(compGuess);
+	console.log(myGuess[0]);
+}
+
+else if ((yourGuess !== myGuess[0]) && (guessesLeft > 0)) {
+	guessesLeft = guessesLeft-1;
+}
+
+else {
+	losses++;
+	guessesLeft = 9;
+	lettersGuessed.length = 0;
+	myGuess.length = 0;
+	var compGuess = myChoices[Math.floor(Math.random() * myChoices.length)];
+	myGuess.push(compGuess);
+	console.log(myGuess[0]);
+}
+
+var html = "<p>Guess the letter:</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Attempts left: " + guessesLeft + "</p>" +
+          "<p>Your guesses : " + lettersGuessed + "</p>";
+ 
+document.querySelector("#psychicGame").innerHTML = html;
+
+}
